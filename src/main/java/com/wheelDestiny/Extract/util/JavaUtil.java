6b3761d84347bbfc6a0e5264b151b0e96dba0365
wheelDestiny <672845190@qq.com>
2020-12-11 17:00:20 +0800

    JavaUtil

---
+++ src/main/java/com/wheelDestiny/Extract/util/JavaUtil.java
@@ -0,0 +1,103 @@
+package com.wheelDestiny.Extract.util;
+
+import cn.wanghaomiao.xpath.model.JXDocument;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.net.URL;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class JavaUtil {
+
+    /**
+     * 获取xpath在dom中对应的元素节点列表
+     *
+     * @param jx        JXDocument
+     * @param xpathList xpath 列表
+     * @return          Elements 列表
+     */
+    private static List<Elements> getElementsList(JXDocument jx, List<String> xpathList) {
+        List<Elements> list = new ArrayList<Elements>();
+        if (xpathList == null) {
+            return list;
+        }
+
+        try {
+            for (String xpath : xpathList) {
+                List sel = jx.sel(xpath);
+                Elements eles = new Elements(sel);
+                list.add(eles);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+
+    private static Pattern IP_PATTERN = Pattern
+            .compile("(\\d{1,3}\\.){3}(\\d{1,3})");
+
+    private final static Set<String> PublicSuffixSet = new HashSet<String>(
+            Arrays.asList("com|org|net|gov|edu|co|tv|mobi|info|asia|xxx|onion|cn|com.cn|edu.cn|gov.cn|net.cn|org.cn|jp|kr|tw|com.hk|hk|com.hk|org.hk|se|com.se|org.se"
+                    .split("\\|")));
+    /**
+     * 获取url的顶级域名
+     */
+    public static String getDomainName(URL url) {
+        String host = url.getHost();
+        if (host.endsWith("."))
+            host = host.substring(0, host.length() - 1);
+        if (IP_PATTERN.matcher(host).matches())
+            return host;
+
+        int index = 0;
+        String candidate = host;
+        for (; index >= 0; ) {
+            index = candidate.indexOf('.');
+            String subCandidate = candidate.substring(index + 1);
+            if (PublicSuffixSet.contains(subCandidate)) {
+                return candidate;
+            }
+            candidate = subCandidate;
+        }
+        return candidate;
+    }
+
+    /**
+     * 判断对象是否为空, 可以对字符串, map, 数组进行判断
+     *
+     * @param obj 需要判断的对象
+     * @return true: 为空, false: 不为空
+     */
+    public static boolean isEmpty(Object obj) {
+        if (obj == null) {
+            return true;
+        } else if (obj instanceof String) {
+            return "".equals(String.valueOf(obj).trim());
+        } else if (obj instanceof Map<?, ?>) {
+            return ((Map<?, ?>) obj).isEmpty();
+        } else if (obj instanceof Collection<?>) {
+            return ((Collection<?>) obj).isEmpty();
+        } else if (obj.getClass().isArray()) {
+            return Array.getLength(obj) == 0;
+        }
+        return false;
+    }
+
+    /**
+     * 判断对象是否为空, 可以对字符串, map, 数组进行判断
+     *
+     * @param obj 需要判断的对象
+     * @return true: 不为空, false: 为空
+     */
+    public static boolean isNotEmpty(Object obj) {
+        return !isEmpty(obj);
+    }
+
+}
